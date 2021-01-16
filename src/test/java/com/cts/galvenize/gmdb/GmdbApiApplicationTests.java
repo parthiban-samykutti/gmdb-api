3d0c5e5e3aba2f12b9a07ec338b8385d150af474
Parthiban Samykutti <parthiban.samykutti@gmail.com>
2021-01-16 10:41:04 -0500

    initial commit

---
+++ src/test/java/com/cts/galvenize/gmdb/GmdbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.galvenize.gmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmdbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
