3a61b99436776071824647f0f655b36d1da34689
tkrishnak <tkrishnagokul@gmail.com>
2021-01-16 16:54:00 -0500

    Added Integration test for findAllMovies with multiple movies database;

--- src/test/java/com/cts/galvenize/gmdb/MovieAPIIntegrationTest.java
+++ src/test/java/com/cts/galvenize/gmdb/MovieAPIIntegrationTest.java
@@ -9,16 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.*;
+import javax.transaction.Transactional;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class MovieAPIIntegrationTest {
 
     @Autowired
@@ -41,14 +41,7 @@ public class MovieAPIIntegrationTest {
     @Test
     @DisplayName("findAllMovies - One value")
     public void testFindAllMoviesWithOneValue() throws Exception {
-        Movie movie = Movie.builder()
-                .title("The Avengers")
-                .actors("Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth")
-                .director("Joss Whedon")
-                .release("2012")
-                .description("Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.")
-                .build();
-        movieRepository.save(movie);
+        createAvengerMovie();
         mockMvc.perform(get("/api/gmdb/movies"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(1))
@@ -58,5 +51,43 @@ public class MovieAPIIntegrationTest {
                 .andExpect(jsonPath("$[0].release").value("2012"))
                 .andExpect(jsonPath("$[0].description").value("Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity."))
                 .andExpect(jsonPath("$[0].rating").doesNotExist());
+    }
+
+    @Test
+    @DisplayName("findAllMovies - Multiple movies")
+    public void testFindAllMoviesWithMultipleValue() throws Exception {
+        createAvengerMovie();
+        createSuperManMovie();
+        mockMvc.perform(get("/api/gmdb/movies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("length()").value(2))
+                .andExpect(jsonPath("$[0].title").value("The Avengers"))
+                .andExpect(jsonPath("$[0].director").value("Joss Whedon"))
+                .andExpect(jsonPath("$[0].actors").value("Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth"))
+                .andExpect(jsonPath("$[0].release").value("2012"))
+                .andExpect(jsonPath("$[0].description").value("Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity."))
+                .andExpect(jsonPath("$[0].rating").doesNotExist());
+    }
+
+    private void createSuperManMovie() {
+        Movie movie = Movie.builder()
+                .title("Superman Returns")
+                .actors("Brandon Routh, Kate Bosworth, Kevin Spacey, James Marsden")
+                .director("Bryan Singer")
+                .release("2006")
+                .description("Superman returns to Earth after spending five years in space examining his homeworld Krypton. But he finds things have changed while he was gone, and he must once again prove himself important to the world.")
+                .build();
+        movieRepository.save(movie);
+    }
+
+    private void createAvengerMovie() {
+        Movie movie = Movie.builder()
+                .title("The Avengers")
+                .actors("Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth")
+                .director("Joss Whedon")
+                .release("2012")
+                .description("Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.")
+                .build();
+        movieRepository.save(movie);
     }
 }
